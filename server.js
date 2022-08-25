4338ef95e48b8dcf48b6f632674b8f53b0904c62
MatValdejao <matvaldejao123@gmail.com>
2022-08-24 21:19:25 -0400

    add role layout working

--- server.js
+++ server.js
@@ -4,145 +4,209 @@ const inquirer = require("inquirer");
 
 // connect to database
 db.connect((err) => {
-    if (err) throw err;
-    console.log("Database connected")
+	if (err) throw err;
+	console.log("Database connected");
 });
 
 const theOptions = () => {
-    inquirer.prompt([
-        // checks what the user wants to do
-        {
-            type: "list",
-            name: "chooseOption",
-            message: "What would you like to do?",
-            choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"]
-        },
-        // performs functionality dependent on the user answer
-    ]).then(({ chooseOption }) => {
-        // calls specific functions
-        switch (chooseOption) {
-            // call cases call a specific function to deal with it
-            case "View All Employees":
-                showEmployees();
-                break;
-            case "View All Roles":
-                showRoles();
-                break;
-            case "Add Employee":
-                addEmployee();
-                break;
-            case "Update Employee Role":
-                updateRole();
-                break;
-            case "Add Role":
-                addRole();
-                break;
-            case "View All Departments":
-                showDepartments();
-                break;
-            case "Add Department":
-                addDepartment();
-                break;
-            default:
-                process.exit()
-        }
-    })
-}
+	inquirer
+		.prompt([
+			// checks what the user wants to do
+			{
+				type: "list",
+				name: "chooseOption",
+				message: "What would you like to do?",
+				choices: [
+					"View All Employees",
+					"Add Employee",
+					"Update Employee Role",
+					"View All Roles",
+					"Add Role",
+					"View All Departments",
+					"Add Department",
+					"Quit",
+				],
+			},
+			// performs functionality dependent on the user answer
+		])
+		.then(({ chooseOption }) => {
+			// calls specific functions
+			switch (chooseOption) {
+				// call cases call a specific function to deal with it
+				case "View All Employees":
+					showEmployees();
+					break;
+				case "View All Roles":
+					showRoles();
+					break;
+				case "Add Employee":
+					addEmployee();
+					break;
+				case "Update Employee Role":
+					updateRole();
+					break;
+				case "Add Role":
+					addRole();
+					break;
+				case "View All Departments":
+					showDepartments();
+					break;
+				case "Add Department":
+					addDepartment();
+					break;
+				default:
+					process.exit();
+			}
+		});
+};
+
+// add role function
+const addRole = () => {
+	// inquire role title, salary, department_id
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "title",
+				message: "What is the name of the role",
+			},
+			{
+				type: "input",
+				name: "salary",
+				message: "What is the salary of the role?",
+			},
+			{
+				type: "list",
+				name: "department",
+				message: "What department does the role belong to?",
+				choices: listDepartments(),
+			},
+		])
+		.then((data) => {
+			const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
+			// what to add to table
+			const params = [data.title, data.salary, null];
+
+			// run query
+			db.query(sql, params, (err, result) => {
+				console.log(`Added ${params[0]} to the database`);
+			});
+		});
+};
 
 // add employee function
 const addEmployee = () => {
-    // prompt user for employee first name, last name, role, and manager
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "first_name",
-            message: "What is the employee's first name?"
-        },
-        {
-            type: "input",
-            name: "last_name",
-            message: "What is the employee's last name?"
-        },
-        {
-            type: "list",
-            name: "role",
-            message: "What is the employee's role?",
-            choices: listRoles()
-        },
-        {
-            type: "list",
-            name: "manager",
-            message: "Who is the employee's manager?",
-            choices: listManagers()
-        }
-    ]).then((data) => {
-        const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
-        const params = [data.first_name, data.last_name, null, null]
-            
-        db.query(sql, params, (err, result) => {
-            console.log(`Added ${params[0]} ${params[1]} to the database.`)
-        })
-    })
-}
+	// prompt user for employee first name, last name, role, and manager
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "first_name",
+				message: "What is the employee's first name?",
+			},
+			{
+				type: "input",
+				name: "last_name",
+				message: "What is the employee's last name?",
+			},
+			{
+				type: "list",
+				name: "role",
+				message: "What is the employee's role?",
+				choices: listRoles(),
+			},
+			{
+				type: "list",
+				name: "manager",
+				message: "Who is the employee's manager?",
+				choices: listManagers(),
+			},
+		])
+		.then((data) => {
+            const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+            // what to add to table
+			const params = [data.first_name, data.last_name, null, null];
+
+            // running query to add to table
+			db.query(sql, params, (err, result) => {
+				console.log(`Added ${params[0]} ${params[1]} to the database.`);
+			});
+		});
+};
 
 const showEmployees = () => {
-    // query
-    const sql = `SELECT * FROM employees`;
+	// query definition
+	const sql = `SELECT * FROM employees`;
 
-    db.query(sql, (err, results) => {
-        console.table(results);
-        theOptions()
-    });
-}
+	db.query(sql, (err, results) => {
+		console.table(results);
+		theOptions();
+	});
+};
 
 const showRoles = () => {
-    // query
-    const sql = `SELECT * FROM roles`;
+	// query definition
+	const sql = `SELECT * FROM roles`;
 
-    db.query(sql, (err, results) => {
-        console.table(results);
-        theOptions()
-    });
-}
+	db.query(sql, (err, results) => {
+		console.table(results);
+		theOptions();
+	});
+};
 
 const showDepartments = () => {
-    // query 
-    const sql = `SELECT * FROM departments`;
+	// query definition
+	const sql = `SELECT * FROM departments`;
 
-    db.query(sql, (err, results) => {
-        console.table(results);
-        theOptions()
-    })
-}
+	db.query(sql, (err, results) => {
+		console.table(results);
+		theOptions();
+	});
+};
 
 const listRoles = () => {
-    // query definition
-    const sql = `SELECT * FROM roles`;
+	// query definition
+	const sql = `SELECT * FROM roles`;
 
-    const titles = [];
-    
-    db.query(sql, (err, results) => {
-        // maps only for role title
-        results.map(({ title }) => {
-            titles.push(title)
-        })
-    })
-    return titles;
-}
+	const titles = [];
+
+	db.query(sql, (err, results) => {
+		// maps only for role title
+		results.map(({ title }) => {
+			titles.push(title);
+		});
+	});
+	return titles;
+};
 
 const listManagers = () => {
-    // query definition
-    const sql = `SELECT * FROM employees`;
+	// query definition
+	const sql = `SELECT * FROM employees`;
 
-    const managers = [];
+	const managers = [];
 
-    db.query(sql, (err, results) => {
-        // maps for employee name
-        results.map(({ first_name, last_name }) => {
-            managers.push(`${first_name} ${last_name}`);
-        })
-    })
-    return managers;
-}
+	db.query(sql, (err, results) => {
+		// maps for employee name
+		results.map(({ first_name, last_name }) => {
+			managers.push(`${first_name} ${last_name}`);
+		});
+	});
+	return managers;
+};
 
-theOptions()
+const listDepartments = () => {
+	// query definition
+	const sql = `SELECT * FROM departments`;
+
+	const departments = [];
+
+	db.query(sql, (err, results) => {
+		// maps only for role title
+		results.map(({ name }) => {
+			departments.push(name);
+		});
+	});
+	return departments;
+};
+
+theOptions();
